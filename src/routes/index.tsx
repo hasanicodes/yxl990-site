2bf92ee15809c005702bb3f3c381525ade0ea5b6
Hasani <62402243+hasanicodes@users.noreply.github.com>
2023-06-30 14:10:00 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,11 +8,7 @@ export default function Home() {
       <h1>Hello world!</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+   YXL990 is a project by Hasani.
       </p>
     </main>
   );
