315400ae5c4126762311b987db2d052ef30abe81
Hasani <62402243+hasanicodes@users.noreply.github.com>
2023-06-30 14:11:31 -0400

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,9 +6,8 @@ export default function NotFound() {
     <main>
       <Title>YXL990</Title>
       <HttpStatusCode code={404} />
-      <h1>Welcome to YXL, full site is coming real soon!</h1>
+      <h1>Full site is coming real soon!</h1>
       <p>
-    YXL990 is a project by Hasani.
       </p>
     </main>
   );
