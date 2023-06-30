30add18a07c9e99efe39c9fddce4abac96a7fac8
Hasani <62402243+hasanicodes@users.noreply.github.com>
2023-06-30 14:06:44 -0400

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,15 +4,11 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title></Title>YXL990</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>Welcome to YXL, full site is coming real soon!</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+    YXL990 is a project by Hasani.
       </p>
     </main>
   );
