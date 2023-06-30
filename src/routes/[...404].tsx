b0e7fec2c7de4faec68ef51e24f2054d98bbb0e0
Hasani <62402243+hasanicodes@users.noreply.github.com>
2023-06-30 14:21:03 -0400

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <main>
       <Title>YXL990</Title>
       <HttpStatusCode code={404} />
-      <h1>Full site is coming real soon!</h1>
+      <h1>Full site is coming soon!</h1>
       <p>
       </p>
     </main>
